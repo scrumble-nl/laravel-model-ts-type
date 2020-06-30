9a635b42a4c65906e0bbbc47eac188f0d0e344fb
Luuk de Weijer <luuk@scrumble.nl>
2020-06-30 21:18:04 +0200

    improved relation property generator logic

--- src/Support/Generators/RelationPropertyGenerator.php
+++ src/Support/Generators/RelationPropertyGenerator.php
@@ -20,23 +20,50 @@ class RelationPropertyGenerator implements IPropertyGenerator
         $withFields = $withProperty->getValue($model);
 
         foreach ($reflectionClass->getMethods() as $method) {
-            if ($method->class === get_class($model) && $method->hasReturnType()) {
-                // TODO: check doc blocks
-                $returnType = $method->getReturnType()->getName();
+            if ($method->class === get_class($model)) {
+                // FIXME: if there only is docblock avaialble, make sure it works for unqualified names aswell
+                $returnType = $this->getReturnType($method);
 
                 if (strpos($returnType, 'Illuminate\Database\Eloquent\Relations') !== false) {
-                    // TODO: Should all relations always be nullable?
+                    // TODO: Should all relations always be nullable? @rico
                     $methodName = $method->getName();
                     $relatedClassSegments = explode('\\', get_class($model->$methodName()->getRelated()));
-                    $relatedClass = end($relatedClassSegments);
-                    $propertyDefinition[snake_case($methodName)] = [
-                        'operator' => in_array($methodName, $withFields) ? ':' : '?:',
-                        'value' =>$relatedClass . (strpos($returnType, 'Many') !== false ? '[]' : '')
-                    ];
+
+                    // TODO: In later stage fix relations for packagized models
+                    if ('App' === $relatedClassSegments[0]) {
+                        $relatedClass = end($relatedClassSegments);
+                        $propertyDefinition[snake_case($methodName)] = [
+                            'operator' => in_array($methodName, $withFields) ? ':' : '?:',
+                            'value' => $relatedClass . (strpos($returnType, 'Many') !== false ? '[]' : '')
+                        ];
+                    }
                 }
             }
         }
 
         return $propertyDefinition;
+    }
+
+    /**
+     * Get return type based on typing or doc block
+     *
+     * @param \ReflectionMethod $method
+     * @return string
+     */
+    private function getReturnType(\ReflectionMethod $method): string
+    {
+        if (null !== ($returnType = $method->getReturnType())) {
+            return $returnType->getName();
+        }
+
+        $docComment = $method->getDocComment();
+        $matches = [];
+        preg_match('/(?<=@return ).+/', $docComment, $matches);
+
+        if (isset($matches[0])) {
+            return $matches[0];
+        }
+
+        return '';
     }
 }

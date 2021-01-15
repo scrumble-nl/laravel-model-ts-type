12d39dd818cd5c75efa227b4863be6ec876a5dff
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-01-15 16:25:18 +0100

    feat: support multiple drivers

--- src/Support/Generators/DatabasePropertyGenerator.php
+++ src/Support/Generators/DatabasePropertyGenerator.php
@@ -6,58 +6,103 @@ namespace Scrumble\TypeGenerator\Support\Generators;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
+use PDO;
 use Scrumble\TypeGenerator\Interfaces\IPropertyGenerator;
 
 class DatabasePropertyGenerator implements IPropertyGenerator
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function getPropertyDefinition(Model $model): array
-    {
-        $propertyDefinition = [];
-        $table = $model->getTable();
-        $fields = DB::select("SHOW FIELDS FROM `$table`");
+  /**
+   * {@inheritDoc}
+   */
+  public function getPropertyDefinition(Model $model): array
+  {
+    $propertyDefinition = [];
+    $table = $model->getTable();
+    $connection = DB::connection($model->getConnection()->getName());
+    $driverName = $model
+      ->getConnection()
+      ->getPDO()
+      ->getAttribute(PDO::ATTR_DRIVER_NAME);
 
-        foreach ($fields as $field) {
-            $propertyDefinition[$field->Field] = $this->formatMysqlType($field);
-        }
+    // Determine fields of table depending on the driver name
+    switch ($driverName) {
+      case 'mysql':
+        $fields = array_map(function ($field) {
+          return [
+            'name' => $field->Field,
+            'type' => $field->Type,
+            'isNullable' => $field->Null === 'YES',
+          ];
+        }, $connection->select("SHOW FIELDS FROM `$table`"));
+        break;
 
-        return $propertyDefinition;
+      case 'pgsql':
+        $fields = array_map(function ($field) {
+            return [
+                'name' => $field->name,
+                'type' => $field->type,
+                'isNullable' => $field->is_nullable === 'YES',
+              ];
+        }, $connection->select("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = `$table`"));
+        break;
+
+      case 'sqlite':
+        $fields = array_map(function ($field) {
+          return [
+            'name' => $field->name,
+            'type' => $field->type,
+            'isNullable' => $field->notnull === 0,
+          ];
+        }, $connection->select("PRAGMA table_info(`$table`)"));
+        break;
+
+      default:
+        throw new \Exception('Driver not supported.');
+        break;
     }
 
-    /**
-     * Format the given mysql field
-     *
-     * @param  \stdClass $field
-     * @return array
-     */
-    public function formatMysqlType(\stdClass $field): array
-    {
-        $type = 'any';
-        $typesToCheck = [
-            'boolean' => MysqlConsts::BOOL_TYPES,
-            'string' => MysqlConsts::STRING_TYPES,
-            'number' => MysqlConsts::NUMBER_TYPES,
-            'string /* Date */' => MysqlConsts::DATE_TYPES
-        ];
-
-        foreach ($typesToCheck as $tsType => $typesToCheck) {
-            foreach ($typesToCheck as $mysqlType) {
-                if (false !== strpos($field->Type, $mysqlType)) {
-                    $type = $tsType;
-                    break;
-                }
-            }
-
-            if ('any' !== $type) {
-                break;
-            }
-        }
-
-        return [
-            'operator' => ':',
-            'value' => $type . ('YES' === $field->Null ? ' | null' : '') . ('any' === $type ? ' // NOT FOUND' : ''),
-        ];
+    foreach ($fields as $field) {
+      $propertyDefinition[$field['name']] = $this->formatDatabaseType($field);
     }
+
+    return $propertyDefinition;
+  }
+
+  /**
+   * Format the given database field
+   *
+   * @param  array $field
+   * @return array
+   */
+  public function formatDatabaseType(array $field): array
+  {
+    $type = 'any';
+    $typesToCheck = [
+      'boolean' => MysqlConsts::BOOL_TYPES,
+      'string' => MysqlConsts::STRING_TYPES,
+      'number' => MysqlConsts::NUMBER_TYPES,
+      'string /* Date */' => MysqlConsts::DATE_TYPES,
+    ];
+
+    foreach ($typesToCheck as $tsType => $typesToCheck) {
+      foreach ($typesToCheck as $databaseType) {
+        if (false !== strpos($field['type'], $databaseType)) {
+          $type = $tsType;
+          break;
+        }
+      }
+
+      if ('any' !== $type) {
+        break;
+      }
+    }
+
+    return [
+      'operator' => ':',
+      'value' =>
+        $type .
+        ('YES' === $field['isNullable'] ? ' | null' : '') .
+        ('any' === $type ? ' // NOT FOUND' : ''),
+    ];
+  }
 }

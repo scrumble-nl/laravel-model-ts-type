3f94994766b4c97b3599757b11acbe74f36ed042
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-01-18 17:52:16 +0100

    fix: update config

--- src/config/laravel-model-ts-type.php
+++ src/config/laravel-model-ts-type.php
@@ -10,4 +10,9 @@ return [
      * The root directory for the type output
      */
     'output_dir' => base_path('resources/js/models'),
+
+    /*
+     * Whether the file name should be formatted to kebab case.
+     */
+    'noKebabCase' => false,
 ];

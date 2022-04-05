1834e00c0a5f1057fe94ac298d7d91bab3a80fe8
Niels Hamelink <nielshamelink@Nielss-MacBook-Pro.local>
2022-04-05 06:59:17 -0700

    Removed comments from code

--- tests/Feature/GeneratesTypesCommandTest.php
+++ tests/Feature/GeneratesTypesCommandTest.php
@@ -28,7 +28,6 @@ class GeneratesTypesCommandTest extends TestCase
         $modelDir = __DIR__ . '/../Models';
         $outputDir = __DIR__ . '/../Output';
 
-        // Check if output is already existing
         foreach ($this->modelList as $modelName) {
             $outputFile = $outputDir . '/' . $modelName . '.d.ts';
 
@@ -37,14 +36,11 @@ class GeneratesTypesCommandTest extends TestCase
             }
         }
 
-        // Try to run the command
         $this->artisan("types:generate --modelDir={$modelDir} --outputDir={$outputDir}")
             ->assertExitCode(0);
 
-        // Reload app to receive new files in cache
         $this->reloadApplication();
 
-        // Check if the generated files exist
         foreach ($this->modelList as $modelName) {
             $outputFile = $outputDir . '/' . $modelName . '.d.ts';
 

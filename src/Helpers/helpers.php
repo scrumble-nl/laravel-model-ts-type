f5e15f80be42360e609f8746d180da109a030828
Luuk de Weijer <luuk@scrumble.nl>
2023-12-22 22:25:45 +0100

    improved typing

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -17,7 +17,7 @@ if (!function_exists('unify_path')) {
 
 if (!function_exists('extractEnumName')) {
     /**
-     * @param  string              $fullyQualifiedName
+     * @param  class-string              $fullyQualifiedName
      * @throws ReflectionException
      * @return string
      */
@@ -29,7 +29,7 @@ if (!function_exists('extractEnumName')) {
 
 if (!function_exists('extractEnumShortName')) {
     /**
-     * @param  string              $fullyQualifiedName
+     * @param  class-string              $fullyQualifiedName
      * @throws ReflectionException
      * @return string
      */
